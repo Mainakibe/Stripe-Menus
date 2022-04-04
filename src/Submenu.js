72132fad5e0839e0e2442d6c7c0d8e024d7a2a71
kibemaina22 <mainakibe@outlook.com>
2022-04-04 09:49:14 +0300

    made changes to sidemenu and context components

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,39 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
 import sublinks from './data';
 
-function useHover() {
-  const [hovered, setHovered] = useState(false);
-  const ref = useRef(null);
 
-  const handleMouseOver = () => setHovered(true);
-  const handleMouseOut = () => setHovered(false);
-
-  useEffect(() => {
-    const node = ref.current;
-
-    if (node) {
-      node.addEventListener('mouseover', handleMouseOver);
-      node.addEventListener('mouseout', handleMouseOut);
-
-      return () => {
-        node.removeEventListener('mouseover', handleMouseOver);
-        node.removeEventListener('mouseout', handleMouseOut);
-      };
-    }
-  }, [ref.current]);
-  return [ref, hovered];
-}
 const Submenu = () => {
-  const [ref, isHovered] = useHover();
   return (
     <aside className="submenu">
       <section>
         {sublinks.map((sub) => {
           const { page } = sub;
           return (
-            <React.Fragment key={page} ref={ref}>
+            <React.Fragment key={page} >
               <h4>{page}</h4>
-              <div className={`${isHovered?''}`}>
+              <div>
                 {sub.links.map((link) => {
                   const { url, label, icon } = link;
                   return (
