3704de9faf57194a58dccc87cfe081fb4fff98c5
kibemaina22 <mainakibe@outlook.com>
2022-04-06 21:46:42 +0300

    added changes to hero,navbar,submenu

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,29 +1,46 @@
 import React, { useState, useRef, useEffect } from 'react';
-import sublinks from './data';
-
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
+  const {
+    isSubMenuHovered,
+    page: { links, page },
+    location,
+    handleSubMenu
+  } = useGlobalContext();
+
+  const [isColumn, setIsColumn] = useState('col-2');
+  const subMenuContainer = useRef(null);
+
+  useEffect(() => {
+    const container = subMenuContainer.current;
+    const { centerPosition, bottomPosition } = location;
+    container.style.left = `${centerPosition}px`;
+    container.style.top = `${bottomPosition}px`;
+    if (links.length === 3) {
+      setIsColumn('col-3');
+    } else if (links.length > 3) {
+      setIsColumn('col-4');
+    }
+  }, [location, links, page]);
   return (
-    <aside className="submenu">
+    <aside
+      className={`${isSubMenuHovered ? 'submenu show' : 'submenu'}`}
+      onMouseOver={handleSubMenu}
+      ref={subMenuContainer}
+    >
       <section>
-        {sublinks.map((sub) => {
-          const { page } = sub;
-          return (
-            <React.Fragment key={page} >
-              <h4>{page}</h4>
-              <div>
-                {sub.links.map((link) => {
-                  const { url, label, icon } = link;
-                  return (
-                    <a href={url} key={label}>
-                      {icon} {label}
-                    </a>
-                  );
-                })}
-              </div>
-            </React.Fragment>
-          );
-        })}
+        <h4>{page}</h4>
+        <div className={`submenu-center ${isColumn}`}>
+          {links.map((link, index) => {
+            const { url, label, icon } = link;
+            return (
+              <a href={url} key={index}>
+                {icon} {label}
+              </a>
+            );
+          })}
+        </div>
       </section>
     </aside>
   );
